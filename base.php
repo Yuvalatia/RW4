f40c87f2510bf8a641383b53fdf261b7f1c0a789
yuvalatia <yodaatia1995@gmail.com>
2019-07-13 03:08:46 +0300

    orc and human player manu added

--- base.php
+++ base.php
@@ -8,19 +8,64 @@ if(!isset($_SESSION['userId'])){
 }
 ?>
 
-<?php
-require ('includes/dbh.php');
+    <div class="row">
+        <div class="col-md-3">
+            <div class="container player-menu human-menu">
+                <div class="row">
+                    <div class="col-sm-12 race-pic">
 
-    echo $_SESSION['userId'];
-    $id = $_SESSION['userId'];
-    echo $_SESSION['userArmy'];
+                     <!--   <div class="orc-pic">
 
-    echo "Untrained: ".$_SESSION['userUntrain'];
-    echo "Warriors: ".$_SESSION['userWarriors'];
-    echo "Workers: ".$_SESSION['userWorkers'];
-    echo "wisdom: ".$_SESSION['userWizdom'];
-    echo "little sowrd 1: ". $_SESSION['wAtt1'];
-?>
+                        </div> -->
+                        <div class="human-pic">
+
+                        </div>
+
+                    </div>
+                    <!--
+                    <div class="col-sm-12 orc-title">
+                        For The Horde !!!
+                        <hr class="menu-line">
+                    </div>
+                    -->
+                    <div class="col-sm-12 human-title">
+                        Humanity Shall Not Fall !
+                        <hr class="menu-line">
+                    </div>
+                    <div class="col-sm-12">
+                        <ul class="list-group">
+                            <li class="list-group-item"><a href="base.php">Base</a></li>
+                            <li class="list-group-item"><a href="base.php">Attack</a></li>
+                            <li class="list-group-item"><a href="base.php">Clan</a></li>
+                            <li class="list-group-item"><a href="base.php">Training</a></li>
+                            <li class="list-group-item"><a href="base.php">Developing</a></li>
+                            <li class="list-group-item"><a href="base.php">Store</a></li>
+                            <li class="list-group-item"><a href="base.php">History</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-md-9">
+
+            <?php
+
+
+            echo $_SESSION['userId'];
+            $id = $_SESSION['userId'];
+            echo $_SESSION['userArmy'];
+
+            echo "Untrained: ".$_SESSION['userUntrain'];
+            echo "Warriors: ".$_SESSION['userWarriors'];
+            echo "Workers: ".$_SESSION['userWorkers'];
+            echo "wisdom: ".$_SESSION['userWizdom'];
+            echo "little sowrd 1: ". $_SESSION['wAtt1'];
+            ?>
+        </div>
+    </div>
+
+
 
 
 <?php
